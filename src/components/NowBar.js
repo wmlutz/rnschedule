3210a475f3fc850e50e3928188bc73b26e7f3064
William Lutz <william.meany.lutz@gmail.com>
2018-11-02 11:44:53 -0400

    test

--- src/components/NowBar.js
+++ src/components/NowBar.js
@@ -61,8 +61,11 @@ class NowBar extends Component {
 }
 
 const sameDay = (d1, d2) => {
+  console.log('d1, d2', d1, d2)
+  return true;
   return d1.getDate() === d2.getDate() &&
   d1.getMonth() === d2.getMonth() && 
   d1.getFullYear() === d2.getFullYear();
 }
+
 export default NowBar
