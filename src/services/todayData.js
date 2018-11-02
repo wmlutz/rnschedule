b3499d0cbd69fa5c2650321b4c5d11a65851e0e5
William Lutz <william.meany.lutz@gmail.com>
2018-11-02 11:47:40 -0400

    test

--- src/services/todayData.js
+++ src/services/todayData.js
@@ -12,6 +12,7 @@ const todayData = (dataArray, date) => {
 }
 
 const sameDay = (d1, d2) => {
+  console.log('today data', d1, d2)
   return d1.getDate() === d2.getDate() &&
   d1.getMonth() === d2.getMonth() && 
   d1.getFullYear() === d2.getFullYear();
